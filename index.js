a2db92683ee6b37295de200d961913106fc4aa2c
Henrik <henrik.saega@voco.ee>
2024-10-10 15:39:05 +0300

    Write data to file

--- index.js
+++ index.js
@@ -5,19 +5,50 @@ const fs = require('node:fs');
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>{
+const readFile = (filename) => {
+    return new Promise((resolve, reject) =>{
+        fs.readFile(filename, "utf8", (err, data)=>{
+                if(err)
+                {
+                    console.error(err);
+                    return;
+                } 
+                const tasks = data.split("\n");
+                resolve(tasks);
+            });
+    });
+};
 
-    fs.readFile('./tasks', "utf8", (err, data) => {
-        if(err){
-            console.error(err);
-            return;
-        } 
-        const tasks = data.split("\n") 
-    
+app.get('/', (req, res) => {
+    readFile('./tasks')
+    .then(tasks => {
     res.render('index',{tasks: tasks});
     });
 });
+
+
+app.post('/', (req, res) => {
+    console.log("Form sent data");
+    let task = req.body.task;   
+
+    readFile('./tasks')
+    .then(tasks =>{
+        tasks.push(task);
+        const data = (tasks.join("\n"));
+        fs.writeFile('./tasks', data, err => {
+            if(err){
+                console.error(err);
+            } else {
+                res.redirect("/");    
+            }
+
+        });
+
+    });
+});
+
 app.listen(3001, () => {
     console.log('Server has started http://localhost:3001/')
-})
+});
