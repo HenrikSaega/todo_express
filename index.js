6cb6e42b717b293b70051fa66d9cc1e13fb3f9e6
Henrik <henrik.saega@voco.ee>
2024-10-18 09:46:04 +0300

    wride data to files

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const readFile = (filename) => {
                     console.error(err);
                     return;
                 } 
-                const tasks = data.split("\n");
+                const tasks = JSON.parse(data)
                 resolve(tasks);
             });
     });
 };
 
 app.get('/', (req, res) => {
-    readFile('./tasks')
+    readFile('./tasks.json')
     .then(tasks => {
     res.render('index',{tasks: tasks});
     });
@@ -33,11 +33,23 @@ app.post('/', (req, res) => {
     console.log("Form sent data");
     let task = req.body.task;   
 
-    readFile('./tasks')
+    readFile('./tasks.json')
     .then(tasks =>{
-        tasks.push(task);
-        const data = (tasks.join("\n"));
-        fs.writeFile('./tasks', data, err => {
+        let index
+        if(tasks.length === 0)
+        {
+            index = 1
+        }  else {
+            index = tasks[tasks.length-1].id +1; 
+        }
+        const newTask ={ 
+            "id" : index,
+            "task" : req.body.task
+        }  
+        console.log(newTask)
+        tasks.push(newTask);
+        const data = JSON.stringify(tasks, null, 2)
+        fs.writeFile('./tasks.json', data, err => {
             if(err){
                 console.error(err);
             } else {
